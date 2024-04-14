327ab818bbfa833a23f02e9ff981c15018195d27
Ben Shelton <122824122+BenActual@users.noreply.github.com>
2024-04-14 15:23:01 -0700

    Add files via upload

---
+++ phase3/linkedlist.h
@@ -0,0 +1,15 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <phase1.h>
+#include <phase2.h>
+#include <phase3.h>
+#include <sems.h>
+#include <libuser.h>
+#include "main.h"
+#include "semtables.h"
+#endif //PHASE_3_TABLES_H
---
+++ phase3/semtables.h
@@ -0,0 +1,14 @@
+#ifndef SEMTABLES_H
+#define SEMTABLES_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <phase1.h>
+#include <phase2.h>
+#include <phase3.h>
+#include <libuser.h>
+#include "main.h"
+#include "linkedlist.h"
+#include "sems.h"
+#endif 
